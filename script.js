54e1c566725334e27b026f30d4c134c392ddf8da
abhinavkrishna05 <abhinavkrishna148@gmail.com>
2026-02-04 14:31:25 +0530

    modified

--- script.js
+++ script.js
@@ -99,20 +99,44 @@
 //     name.value=""
 // })
 
-console.log("Starting homework...");
+// console.log("Starting homework...");
 
-setTimeout(() => {
-    console.log("Homework done!");
-    console.log("Starting dinner...");
+// setTimeout(() => {
+//     console.log("Homework done!");
+//     console.log("Starting dinner...");
 
+//     setTimeout(() => {
+//         console.log("Dinner done!");
+//         console.log("Getting ready to go out...");
+        
+//         setTimeout(() => {
+//             console.log("Go to the playground!");
+//         }, 1000); // after dinner
+
+//     },1500); // dinner time
+
+// }, 2000); // homework time
+
+function finishHomework(){
+    console.log("Starting homework...");
+        setTimeout(() => {
+          console.log("Homework done!");
+          useCallback();  
+        }, 2000);
+    }
+function eatDinner(){
+    console.log("Starting dinner...");  
     setTimeout(() => {
         console.log("Dinner done!");
-        console.log("Getting ready to go out...");
-        
-        setTimeout(() => {
-            console.log("Go to the playground!");
-        }, 1000); // after dinner
+        Callback();
+    }, 1500);
+}   
+function goToPlayground(){
+        console.log("Go to the playground!");
+    }
 
-    },1500); // dinner time
-
-}, 2000); // homework time
+finishHomework(()=>{
+    eatDinner(()=>{
+        goToPlayground();
+    });
+})
